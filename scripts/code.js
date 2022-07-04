cc6521030845c27c3bced54208ee3576048f701b
Sibongile <rwayisibongile@gmail.com>
2022-07-04 12:17:25 +0200

    first coommit

---
+++ scripts/code.js
@@ -0,0 +1,14 @@
+
+
+const app = Vue.createApp({
+    // data(){
+
+    // },
+    data: function() {
+        return {
+            fname: 'Sibongile',
+            lname: 'Rwayi'
+            }       
+    }
+});
+app.mount('#Fullname');
